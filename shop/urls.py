9e34f4f102546e94db3d8bc71ac8a889e2d7858b
Dxtag <dxtagp+github@gmail.com>
2020-06-24 22:17:44 +0200

    .

--- shop/urls.py
+++ shop/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import index_view, product_view, basket_view
+from .views import index_view, product_view, basket_view, add_to_basket, set_quantity, staff_orders_view
 
 app_name = "shop"
 
 urlpatterns = [
     path("", index_view.as_view(), name="index"),
-    path("product/<int:pk>", product_view.as_view(), name="product"),
-    path("basket", basket_view, name="basket")
+    path("product/<int:pk>", product_view, name="product"),
+    path("basket", basket_view, name="basket"),
+    path("product/<int:pk>/add:<int:quantity>", add_to_basket, name="add_to_basket"),
+    path("basket/<int:pk>/set:<int:quantity>", set_quantity, name="set_quantity"),
+    path("staff/orders", staff_orders_view, name="staff_orders")
 ]
